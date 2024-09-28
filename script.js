3d4c6b06de81d59515f87d5f3f475e7e0df88fd0
rizallapta <rizalapta23@gmail.com>
2024-09-28 19:07:41 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("signupButton").addEventListener("click", function(event) {
+        event.preventDefault(); // Prevent any default form submission
+
+        // Redirect to YouTube
+        window.location.href = " https://rizallapta.github.io/Portofolio/";
+    });
+});
