c4640d41a7e47b8105875b2a30035cdfc79bac83
rizallapta <153175953+rizallapta@users.noreply.github.com>
2024-10-23 09:55:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("signupButton").addEventListener("click", function(event) {
         event.preventDefault(); // Prevent any default form submission
 
-        // Redirect to YouTube
         window.location.href = " https://rizallapta.github.io/Portofolio/";
     });
 });
